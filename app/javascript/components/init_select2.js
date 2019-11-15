f5bedb730e0897e2d40c509366948f3bdeb58020
Quentin Blanc <qblanc@hotmail.com>
2019-11-15 12:09:38 +0100

    transfer

--- app/javascript/components/init_select2.js
+++ app/javascript/components/init_select2.js
@@ -2,8 +2,7 @@ import $ from 'jquery';
 import 'select2';
 
 const initSelect2 = () => {
-  console.log("couocu")
-  $('#dose_ingredient_id').select2();
+  $('.select2').select2();
 };
 
 export { initSelect2 };
