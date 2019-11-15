ed4fb4e990eb4409840c4713aae912a2c202fdaf
Quentin Blanc <qblanc@hotmail.com>
2019-11-15 16:26:37 +0100

    new seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,15 @@ Ingredient.destroy_all
 puts 'Ingredients deleted'
 
 puts 'Creating ingredients...'
-url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-ingredient_list = JSON.parse(open(url).read)
-ingredient_list['drinks'].each do |ingredient|
-  Ingredient.create!(name: ingredient['strIngredient1'])
+ingredient_ids = (1..600).to_a
+ingredient_ids.each do |ingredient_id|
+  url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?iid=#{ingredient_id}"
+  ingredient = JSON.parse(open(url).read)
+  if ingrdient['ingredients'].!nil?
+    ingredient['ingredients'].each do |ingredient_name|
+      Ingredient.create!(name: ingredient_name['strIngredient'])
+    end
+  end
 end
 puts 'Ingredients created'
 
